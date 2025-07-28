704c57a7f197cd215e844ca46de9f2c1ae40d33b
Cristiano Lages <cristianolagesf@hotmail.com>
2025-07-28 16:37:45 -0400

    call  the init_db

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from view import main_menu
+from model import database
 from control.menu_control import main_menu_controller
 
 
 main_menu.menu()
+database.init_db()
 option = int(input("Option: "))
+
 main_menu_controller.menu_controller(option)
